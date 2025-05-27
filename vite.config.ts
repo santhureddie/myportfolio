e13919fb6112319e21c8c46c6da19963cc578805
Santhosh Kumar Reddy Jampana <61430089+santhureddie@users.noreply.github.com>
2025-05-27 02:35:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  base: '/myportfolio/' // replace with '/<repo-name>/'
 });
